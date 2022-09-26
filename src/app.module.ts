85471621919db8a7678c4d3fe14cecff8c49149c
Md. Sakibul Alam <mdskrumi@gmail.com>
2022-09-26 06:50:48 +0600

    Add sqlite and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { UserModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
 import { AuthDto } from './dtos';
 
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) {}
   async signup(body: AuthDto) {
-    return {
-      ...body,
-      hash: await this.hashPassword(body.password),
-    };
+    try {
+      const hash = await this.hashPassword(body.password);
+      const user = { email: body.email, password: hash };
+      return await this.prismaService.user.create({ data: user });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credential taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
