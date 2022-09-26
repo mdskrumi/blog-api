b74b748e36aa8927209ca6eb3bfc302162c0d894
Md. Sakibul Alam <mdskrumi@gmail.com>
2022-09-26 06:06:08 +0600

    Add class validators and class transformers and hashed password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dtos';
 
-@Controller('user')
+@Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('signup')
-  signup() {
-    return {
-      task: 'sign up',
-    };
+  signup(@Body() body: AuthDto) {
+    return this.authService.signup(body);
   }
 
   @Post('signin')
   signin() {
-    return {
-      task: 'sign in',
-    };
+    return this.authService.signin();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dtos';
+
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signup(body: AuthDto) {
+    return {
+      ...body,
+      hash: await this.hashPassword(body.password),
+    };
+  }
+
+  signin() {
+    return {
+      task: 'sign in',
+    };
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    return salt + '.' + hash.toString('hex');
+  }
+}
